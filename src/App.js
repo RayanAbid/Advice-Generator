6169c5daf1c2a6c9bd3cafb8a77722db5662082e
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-07-30 21:06:26 +0500

    Update data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    setLoading(true);
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    await fetch("https://api.adviceslip.com/advice", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.slip.advice);
+        setAdvice(result.slip.advice);
+        setLoading(false);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Get a Random Advice 🔥</h1>
+        {loading ? <p>loading...</p> : <p>{advice}</p>}
+
+        <button
+          className="btn"
+          onClick={() => {
+            fetchData();
+          }}
         >
-          Learn React
-        </a>
+          Get another advice
+        </button>
       </header>
     </div>
   );
